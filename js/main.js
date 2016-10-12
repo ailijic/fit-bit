f5cd9066e53331e57eb929b49da8c986d54f1ec0
Andrew Ilijic <ailijic@gmail.com>
2016-10-12 18:50:51 -0400

    change x axis lables to dates

--- js/main.js
+++ js/main.js
@@ -86,22 +86,14 @@ function extractPoints (jsonObj) {
 }
 function renderGraph (points) {
   let ctx = $("#myChart")
-  var myChart = new Chart(ctx, {
+  let myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: ["2016-08-03", "2016-08-09", "2016-08-15", "2016-08-21", "2016-08-27"],
     datasets: [{
-      label: '# of Votes',
-//      data: [12, 7, 3, 5, 2, 3],
+      label: 'Steps',
       data: points,
-      backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-      ],
+      backgroundColor: "rgba(153,255,51,0.4)",
       borderColor: [
         'rgba(255,99,132,1)',
         'rgba(54, 162, 235, 1)',
