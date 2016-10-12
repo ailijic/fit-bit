7467023884e39743c20e7ef5a6378ec8d1f3d88f
Andrew Ilijic <ailijic@gmail.com>
2016-10-12 16:35:09 -0400

    implement fetch

--- js/main.js
+++ js/main.js
@@ -1,45 +1,63 @@
 main()
 function main () {
 'use strict'
+ // const fetch = require('node-fetch')
+
   const authObj = getAuthObj()
-  const dataObj = requestData(authObj)
+  const jsonPromise = requestData(authObj)
   const dataToGraph = extractGraphPoints(dataObj)
   
   $(document).ready(function () {
     removeSignInIfLoggedIn(authObj)
-    renderGraph(dataToGraph)
+    resolvePromise(jsonPromise, graphJson)  
+      // renderGraph(dataToGraph)
   })
 
-function reqListener () {
-  console.log(JSON.parse(this.responseText))
-  
+/////////////////////////////////////////////////////////////////////////////
+// const jsonPromise = requestData(authObj)
+// resolvePromise(jsonPromise, graphJson)
+function graphJson (jsonObj) {
+  console.log(jsonObj)
 }
-
-function buildUrl () {
+/////////////////////////////////////////////////////////////////////////////
+function requestData (authObj) {
+  // Auth data
+  const headerType = authObj.token_type
+  const token = authObj.access_token
+  const userId = authObj.user_id
+  // variables to select what data you want back
+  const recordToGet = 'activities/steps'
+  const baseDate = '2016-09-01'
+  const period = '30d'
+  const baseUrl = 'https://api.fitbit.com/1/user'
+  const url = buildJsonUrl(baseUrl, userId, recordToGet, 'date', baseDate, period)
+  const headerObj = { 
+    method: 'get',
+    headers: { 'Authorization': headerType + ' ' + token }
+  }
+  const fetchPromise = fetch(url, headerObj)
+  return fetchPromise
+}
+function resolvePromise (promise, nextFunc) {
+  promise.then( (response) => {
+    if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' + response.status)
+      return
+    }
+    else { response.json().then( (data) => nextFunc(data) )
+    }
+  })
+  .catch( (err) => { console.log('Fetch Error :-S', err) })
+}
+function buildJsonUrl () {
   let retUrl = ''
-  let args = arguments
+  const args = arguments
   for (let i in args) {
     retUrl = retUrl + args[i] + '/'
   }
   retUrl = retUrl.slice(0,-1) // Use slice cuz url has extra '/'
   retUrl += '.json'
   return retUrl
-}
-
-
-function requestData (authObj) {
-  let authToken = authObj.access_token // 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0TTZGQk4iLCJhdWQiOiIyMjgyODMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNDc2Mzc0MTg4LCJpYXQiOjE0NzYyODk5NTF9.sV3JmVlDn6xP7P4zcgFtr5BoJguwQ1D4a7Xa5ZkGZeo'
-  let userId = '4M6FBN'
-  let recordToGet = 'activities/steps'
-  let baseDate = '2016-09-01'
-  let period = '30d'
-  let baseUrl = 'https://api.fitbit.com/1/user'
-  let url = buildUrl(baseUrl, userId, recordToGet, 'date', baseDate, period)
-  let oReq = new XMLHttpRequest()
-  oReq.addEventListener('load', reqListener)
-  oReq.open('GET', url)
-  oReq.setRequestHeader('Authorization', 'Bearer ' + authToken)
-  oReq.send()
 }
 function removeSignInIfLoggedIn (authObj) {
   if (typeof authObj.access_token !== 'undefined') {
--- js/test-promise.js
+++ js/test-promise.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
-/////////////////////////////////////////////////////////////////////////////
+
 const authObj = {
   path: 'https://ailijic.github.io/fit-bit/',                                
   access_token: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0TTZGQk4iLCJhdWQiOiIyMjgyODMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNDc2Mzc0MTg4LCJpYXQiOjE0NzYyOTcxOTN9.bsjETO_78rxeJzQl9YMAzip0_T2rKaDUhS_e-NKQ4sc',
@@ -8,42 +8,7 @@ const authObj = {
   token_type: 'Bearer',
   expires_in: '8003'
 }
-/////////////////////////////////////////////////////////////////////////////
-const jsonPromise = requestData(authObj)
-resolvePromise(jsonPromise, graphJson)
-function graphJson (jsonObj) {
-  console.log(jsonObj)
-}
-/////////////////////////////////////////////////////////////////////////////
-function requestData (authObj) {
-  // Auth data
-  const headerType = authObj.token_type
-  const token = authObj.access_token
-  const userId = authObj.user_id
-  // variables to select what data you want back
-  const recordToGet = 'activities/steps'
-  const baseDate = '2016-09-01'
-  const period = '30d'
-  const baseUrl = 'https://api.fitbit.com/1/user'
-  const url = buildJsonUrl(baseUrl, userId, recordToGet, 'date', baseDate, period)
-  const headerObj = { 
-    method: 'get',
-    headers: { 'Authorization': headerType + ' ' + token }
-  }
-  const fetchPromise = fetch(url, headerObj)
-  return fetchPromise
-}
-function resolvePromise (promise, nextFunc) {
-  promise.then( (response) => {
-    if (response.status !== 200) {
-      console.log('Looks like there was a problem. Status Code: ' + response.status)
-      return
-    }
-    else { response.json().then( (data) => nextFunc(data) )
-    }
-  })
-  .catch( (err) => { console.log('Fetch Error :-S', err) })
-}
+
 function buildJsonUrl () {
   let retUrl = ''
   const args = arguments
@@ -54,3 +19,30 @@ function buildJsonUrl () {
   retUrl += '.json'
   return retUrl
 }
+
+function requestData (authObj) {
+  // Auth data
+  const headerType = authObj.token_type
+  const token = authObj.access_token
+  const userId = authObj.user_id
+
+  // variables to select what data you want back
+  const recordToGet = 'activities/steps'
+  const baseDate = '2016-09-01'
+  const period = '30d'
+  const baseUrl = 'https://api.fitbit.com/1/user'
+  const url = buildJsonUrl(baseUrl, userId, recordToGet, 'date', baseDate, period)
+  const headerObj = { method: 'get',
+    headers: { 'Authorization': headerType + ' ' + token }
+  }
+  const fetchPromise = fetch(url, headerObj)
+  const promise2 = fetchPromise
+  /*
+  promise2.then( (res) => {
+    res.json().then( (data) => { console.log(data) })
+  })
+  */
+  return fetchPromise
+}
+/////////////////////////////////////////////////////////////////////////////
+requestData(authObj)
